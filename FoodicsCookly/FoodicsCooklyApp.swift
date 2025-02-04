504188d66a165585e07d3d585a172b0b145cd246
alaaiosdev <alaa.ajoury94@gmail.com>
2025-02-04 15:35:39 +0300

    Initial Commit

---
+++ FoodicsCookly/FoodicsCooklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodicsCooklyApp.swift
+//  FoodicsCookly
+//
+//  Created by Alaa Abu-Taha on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodicsCooklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
